8890b069fb4d10bfd3a8b8b6deb355f538fd42c0
ashoss <galaxyash1988@gmail.com>
2024-05-14 16:51:05 +0100

    redfin: properly remove hbm

--- redfin-vendor.mk
+++ redfin-vendor.mk
@@ -110,7 +110,6 @@ PRODUCT_PACKAGES += \
     EuiccGoogleOverlay \
     EuiccSupportPixel \
     EuiccSupportPixelPermissions \
-    HbmSVManager \
     MyVerizonServices \
     OBDM_Permissions \
     RilConfigService \
